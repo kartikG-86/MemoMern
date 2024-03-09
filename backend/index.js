371f0d818336217dba2ff33ca4379f124766f42a
Kartik Goyal <kartikgoyal0852@gmail.com>
2024-03-09 14:01:59 +0530

    Allow Cors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const port = 5000;
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow CORS from all sources
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader(
     "Access-Control-Allow-Headers",
